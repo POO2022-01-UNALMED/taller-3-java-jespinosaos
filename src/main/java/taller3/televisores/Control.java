6141e0746a3a3d980328ce5eb395e320a976f6c5
jespinosaos <101433694+jespinosaos@users.noreply.github.com>
2022-03-29 00:08:59 -0500

    taller3

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,5 +1,44 @@
 package taller3.televisores;
 
 public class Control {
+	TV tv;
+	public void turnOn(){
+		this.tv.turnOn(); 
+	}
+	
+	public void turnOff(){
+		this.tv.turnOff();
+	}
+	
+	public void canalUp(){
+		this.tv.canalUp();
+	}
+	
+	public void canalDown(){
+		this.tv.canalDown();
+	}
+	
+	public void volumenUp(){
+		this.tv.volumenUp();
+	}
+	
+	public void volumenDown(){
+		this.tv.volumenDown();
+	}
+	
+	public void setCanal(int canal){
+		this.tv.setCanal(canal);
+	}
+	
+	public void enlazar(tv) {
+		
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
 
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
 }
